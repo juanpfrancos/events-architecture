0c6417e02c3f79f816a9144092259279bcb4b88d
Juanpfrancos <juanpanime@gmail.com>
2024-12-11 15:13:30 -0500

    update: Updated environment variable name

--- events_service/config.py
+++ events_service/config.py
@@ -4,7 +4,7 @@ import os
 
 
 load_dotenv()
-DATABASE_URL = f"{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DB")}"
+DATABASE_URL = f"{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("EVENTS_DB")}"
 
 
 class Settings(BaseSettings):
